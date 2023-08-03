e44c3f3e3e260da5ab5c06d1c0e0ca1fa55bc633
Doreen <doreenoms6699@gmail.com>
2023-08-03 21:27:37 +0300

    submit solution

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
- * binary_tree_preorder - Goes through a binary tree using in-order traversal.
+ * binary_tree_inorder - Goes through a binary tree using in-order traversal.
  *
  * @tree: A pointer to the root node of the tree to traverse.
  * @func: A pointer to a function to call for each node.
